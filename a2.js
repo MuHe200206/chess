733f14497ecb48b9db9b1f17500d46cd3a7cccc0
MuHe200206 <99844456+MuHe200206@users.noreply.github.com>
2023-10-23 23:49:55 -0600

    Add some commit

--- a2.js
+++ a2.js
@@ -3,10 +3,34 @@
 // Assignment 2 
 // Name: Mu He
 // UCID: 30117178
+// to the game page
 function gamePage(){
     window.location.href = "gamePage.html";
 }
 
+// to the home page
 function homePage(){
     window.location.href = "a2.html"
+}
+
+// rule and logic of chess game
+let currentPlayer = 0;
+
+// function trace moveing
+function movePiece(){
+    // check moving piece and update location
+    // switch the player
+}
+
+// main loop for game
+while(true){
+    movePiece();
+    // check whether the game is over
+    // if(current player win){
+    //     break;
+    // }
+    if(currentPlayer==0)
+        currentPlayer = 1;
+    else
+        currentPlayer = 0
 }
