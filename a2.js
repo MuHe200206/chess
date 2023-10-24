a8fd01640f9c7419ca539f6b99e2db18e8c2fa5f
MuHe200206 <99844456+MuHe200206@users.noreply.github.com>
2023-10-23 23:24:05 -0600

    Add some new UI

--- a2.js
+++ a2.js
@@ -1,4 +1,12 @@
-
+// Course: SENG 513 
+// Date: OCT 23, 2023 
+// Assignment 2 
+// Name: Mu He
+// UCID: 30117178
 function gamePage(){
     window.location.href = "gamePage.html";
+}
+
+function homePage(){
+    window.location.href = "a2.html"
 }
